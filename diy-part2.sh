93415b612fd45522f97b00c7e38a9ea707db89c5
slcun <slcun@live.com>
2023-03-28 13:36:07 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,5 +10,5 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.17.1）
+#sed -i 's/192.168.1.1/192.168.17.1/g' package/base-files/files/bin/config_generate
